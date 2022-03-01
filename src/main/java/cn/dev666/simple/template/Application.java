28963ccc1fe75cbc811670d838544974968f2040
kou <dev@learncoding.cn>
2022-03-01 10:20:57 +0800

    准备工作提交

--- src/main/java/cn/dev666/simple/template/Application.java
+++ src/main/java/cn/dev666/simple/template/Application.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
-public class SimpleSpringBootMybatisPlusTemplateApplication {
+public class Application {
 
     @GetMapping("/")
     public String index(){
@@ -15,7 +15,7 @@ public class SimpleSpringBootMybatisPlusTemplateApplication {
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(SimpleSpringBootMybatisPlusTemplateApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
