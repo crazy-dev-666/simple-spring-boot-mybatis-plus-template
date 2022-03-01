3cde720f1e87c574b091de9090ac13ac77746b43
kou <dev@learncoding.cn>
2022-03-01 10:04:24 +0800

    init

---
+++ src/test/java/cn/dev666/simple/template/SimpleSpringBootMybatisPlusTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.dev666.simple.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringBootMybatisPlusTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
