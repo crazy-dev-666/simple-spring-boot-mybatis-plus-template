a81102b2ef738651ec21d876caefca6c076b7c85
kou <dev@learncoding.cn>
2022-03-04 08:56:58 +0800

    最小化

--- src/main/java/cn/dev666/simple/template/ctrl/UserCtrl.java
+++ src/main/java/cn/dev666/simple/template/ctrl/UserCtrl.java
@@ -14,7 +14,6 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
