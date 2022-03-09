893ae56a32962392364a2210d2615730953332c9
kou <dev@learncoding.cn>
2022-03-09 09:26:50 +0800

    鉴权&会话

--- src/main/java/cn/dev666/simple/template/config/RedisConfig.java
+++ src/main/java/cn/dev666/simple/template/config/RedisConfig.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.BeanClassLoaderAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.security.jackson2.SecurityJackson2Modules;
@@ -21,14 +20,6 @@ public class RedisConfig implements BeanClassLoaderAware {
 	public RedisSerializer<Object> springSessionDefaultRedisSerializer() {
 		return new GenericJackson2JsonRedisSerializer(objectMapper());
 	}
-
-    /**
-     * redis 连接池
-     */
-    @Bean
-    public LettuceConnectionFactory connectionFactory() {
-        return new LettuceConnectionFactory();
-    }
 
 	private ObjectMapper objectMapper() {
 		ObjectMapper mapper = new ObjectMapper();
